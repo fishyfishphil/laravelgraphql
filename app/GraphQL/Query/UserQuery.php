7cd8e790daecb565eccee5a8f07aebde12002008
Phillip <phillipdebruin1@gmail.com>
2018-03-01 15:26:31 +0200

    Update query comments

--- app/GraphQL/Query/UserQuery.php
+++ app/GraphQL/Query/UserQuery.php
@@ -8,18 +8,31 @@ use GraphQL;
 use GraphQL\Type\Definition\ResolveInfo;
 use GraphQL\Type\Definition\Type;
 
+/**
+ * Class UserQuery
+ * @package App\GraphQL\Query
+ */
 class UserQuery extends Query
 {
+    /**
+     * @var array
+     */
     protected $attributes = [
         'name' => 'UserQuery',
         'description' => 'A User query',
     ];
 
+    /**
+     * @return null
+     */
     public function type()
     {
         return GraphQL::type('User'); //If error put: UserType
     }
 
+    /**
+     * @return array
+     */
     public function args()
     {
         return [
@@ -29,6 +42,13 @@ class UserQuery extends Query
         ];
     }
 
+    /**
+     * @param $root
+     * @param $args
+     * @param $context
+     * @param ResolveInfo $info
+     * @return mixed
+     */
     public function resolve($root, $args, $context, ResolveInfo $info)
     {
         return User::find($args['id']);
